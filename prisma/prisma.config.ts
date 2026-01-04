918451787efc20b38f6f05556262968161edf4ac
J404Simpson <jasonjamessimpson@gmail.com>
2026-01-05 00:20:33 +1300

    Deployment error workthrough

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,5 +1,7 @@
-export default {
+import { defineConfig } from '@prisma/config';
+
+export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL ?? "postgresql://default:default@localhost:5432/default",
+    url: process.env.DATABASE_URL ?? 'postgresql://default:default@localhost:5432/default',
   },
-};
+});
